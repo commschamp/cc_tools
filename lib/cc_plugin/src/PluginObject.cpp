d0de5356ca3d85e3ce2d1d8bd31ba65f844d6f8f
Alex Robenko <arobenko@gmail.com>
2021-03-09 08:13:46 +1000

    Preparation of the plugin library.

---
+++ lib/cc_plugin/src/PluginObject.cpp
@@ -0,0 +1,23 @@
+#include "cc_tools/cc_plugin/PluginObject.h"
+
+namespace cc_tools
+{
+
+namespace cc_plugin
+{
+
+PluginObject::PluginObject(QObject* p) : 
+    Base(p)
+{
+}
+
+PluginObject::~PluginObject() = default;
+
+Type PluginObject::getType() const
+{
+    return getTypeImpl();
+}
+
+} // namespace cc_plugin
+
+} // namespace cc_tools
