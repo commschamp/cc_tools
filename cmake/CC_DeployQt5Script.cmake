802b7e0bdbe0d0b3bba929db92049bbe2846b731
Alex Robenko <arobenko@gmail.com>
2021-03-26 17:58:07 +1000

    Testing deploy improvements.

--- cmake/CC_DeployQt5Script.cmake
+++ cmake/CC_DeployQt5Script.cmake
@@ -3,7 +3,9 @@
 # Expected input variables
 # CC_QT_DIR - Directory of Qt5 installation
 # CC_DEPLOY_DIR - Directory where all Qt5 libraries need to be deployed
-# CC_PARSE_DIR - Directory that need to parsed for deployment dependencies
+# CC_BIN_DIR - Directory containing executables
+# CC_PLUGIN_DIR - Directory containing plugin dlls
+
 # CC_QML_DIR - Directory where all qml files are located
 
 if (NOT WIN32)
@@ -18,7 +20,7 @@ if ("${CC_DEPLOY_DIR}" STREQUAL "")
     message (FATAL_ERROR "Deployment directory hasn't been provided.")
 endif()
 
-if ("${CC_PARSE_DIR}" STREQUAL "")
+if ("${CC_BIN_DIR}" STREQUAL "")
     message (FATAL_ERROR "Directory to parse hasn't been provided.")
 endif()
 
@@ -33,12 +35,11 @@ if (NOT "${CC_QML_DIR}" STREQUAL "")
     set (qml_arg --qmldir)
 endif ()
 
-file (GLOB binaries "${CC_PARSE_DIR}/*.exe")
+file (GLOB binaries "${CC_BIN_DIR}/*.exe")
+file (GLOB plugins "${CC_PLUGIN_DIR}/*.dll")
 
-foreach (f ${binaries} ${plugins})
-    execute_process (
-        COMMAND ${deploy_exe} --dir ${CC_DEPLOY_DIR} ${qml_arg} ${CC_QML_DIR} ${f}
-    )
-endforeach ()
+execute_process (
+    COMMAND ${deploy_exe} --dir ${CC_DEPLOY_DIR} ${qml_arg} ${CC_QML_DIR} ${binaries} ${plugins}
+)
 
 
